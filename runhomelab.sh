05120b675d89b0add8d0a19b1f2a8905c116cfbc
Eduardo De La Cruz <eduardo.delacruz42@uabc.edu.mx>
2026-08-11 00:42:09 -0700

    Update containers as no-root

--- runhomelab.sh
+++ runhomelab.sh
@@ -5,17 +5,21 @@ BASE_DIR="$(pwd)"
 PROXY_COMPOSE="$BASE_DIR/proxy/compose.yml"
 DATABASE_COMPOSE="$BASE_DIR/database/compose.yml"
 SERVICES_COMPOSE="$BASE_DIR/services/compose.yml"
+MONITORING_COMPOSE="$BASE_DIR/monitoring/compose.yml"
 
 echo "=================================================="
 echo "Starting ordered Homelab deployment"
 echo "   Detected root: $BASE_DIR"
 echo "=================================================="
 
-echo "[1/3] Starting Database..."
+echo "[1/4] Starting Database..."
 docker compose -f "$DATABASE_COMPOSE" up
 
-echo "[2/3] Starting Services..."
+echo "[2/4] Starting Monitoring..."
+docker compose -f "$MONITORING_COMPOSE" up
+
+echo "[3/4] Starting Services..."
 docker compose -f "$SERVICES_COMPOSE" up
 
-echo "[3/3] Starting Proxy..."
+echo "[4/4] Starting Proxy..."
 docker compose -f "$PROXY_COMPOSE" up
--- stophomelab.sh
+++ stophomelab.sh
@@ -5,19 +5,23 @@ BASE_DIR="$(pwd)"
 PROXY_COMPOSE="$BASE_DIR/proxy/compose.yml"
 DATABASE_COMPOSE="$BASE_DIR/database/compose.yml"
 SERVICES_COMPOSE="$BASE_DIR/services/compose.yml"
+MONITORING_COMPOSE="$BASE_DIR/monitoring/compose.yml"
 
 echo "=================================================="
 echo "Stopping Homelab infrastructure in reverse order"
 echo "   Detected root: $BASE_DIR"
 echo "=================================================="
 
-echo "[1/3] Stopping Proxy..."
+echo "[1/4] Stopping Proxy..."
 docker compose -f "$PROXY_COMPOSE" down
 
-echo "[2/3] Stopping Services..."
+echo "[2/4] Stopping Services..."
 docker compose -f "$SERVICES_COMPOSE" down
 
-echo "[3/3] Stopping Database..."
+echo "[2/4] Stopping Monitoring..."
+docker compose -f "$MONITORING_COMPOSE" down
+
+echo "[3/4] Stopping Database..."
 docker compose -f "$DATABASE_COMPOSE" down
 
 echo "=================================================="
