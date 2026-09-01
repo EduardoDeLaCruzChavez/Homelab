2a0bcccbac445b9cdb341d4729ba447920b6c121
Eduardo De La Cruz <eduardo.delacruz42@uabc.edu.mx>
2026-08-31 20:10:37 -0700

    Added openid pgadmin and fix cookie errors

---
+++ database/config/config_local.py
@@ -0,0 +1,27 @@
+import os
+
+with open('/run/secrets/pgadmin_secret', 'r') as f:
+    OIDC_CLIENT_SECRET = f.read().strip()
+
+AUTHELIA_DOMAIN = 'auth.homelab.cz:8989'
+
+AUTHENTICATION_SOURCES = ['oauth2', 'internal']
+OAUTH2_AUTO_CREATE_USER = True
+OAUTH2_CONFIG = [{
+    'OAUTH2_NAME': 'Authelia',
+    'OAUTH2_DISPLAY_NAME': 'Authelia',
+    'OAUTH2_CLIENT_ID': 'pgadmin',
+    'OAUTH2_CLIENT_SECRET': OIDC_CLIENT_SECRET,
+    'OAUTH2_API_BASE_URL': f'https://{AUTHELIA_DOMAIN}',
+    'OAUTH2_AUTHORIZATION_URL': f'https://{AUTHELIA_DOMAIN}/api/oidc/authorization',
+    'OAUTH2_TOKEN_URL': f'https://{AUTHELIA_DOMAIN}/api/oidc/token',
+    'OAUTH2_USERINFO_ENDPOINT': f'https://{AUTHELIA_DOMAIN}/api/oidc/userinfo',
+    'OAUTH2_SERVER_METADATA_URL': f'https://{AUTHELIA_DOMAIN}/.well-known/openid-configuration',
+    'OAUTH2_SCOPE': 'openid email profile',
+    'OAUTH2_USERNAME_CLAIM': 'email',
+    'OAUTH2_ICON': 'fa-openid',
+    'OAUTH2_BUTTON_COLOR': '#000000',
+    'OAUTH2_CHALLENGE_METHOD': 'S256',
+    'OAUTH2_RESPONSE_TYPE': 'code',
+}]
+
